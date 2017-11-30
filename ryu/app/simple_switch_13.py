a47aa4dc01218d64b16acd7e2e851cee158103d3
秋田海人 <kaito@akitakaininnoMac-Pro.local>
2017-11-30 23:21:20 +0900

    timestamp char

--- ryu/app/simple_switch_13.py
+++ ryu/app/simple_switch_13.py
@@ -89,6 +89,7 @@ class SimpleSwitch13(app_manager.RyuApp):
         self.mac_to_port.setdefault(dpid, {})
 
         self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+        self.logger.info(datapath.address[0])
 
         # learn a mac address to avoid FLOOD next time.
         self.mac_to_port[dpid][src] = in_port
