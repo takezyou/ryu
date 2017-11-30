d53492628ebbd0e4fa7d7cbe25119b31c70919a0
秋田海人 <kaito@akitakaininnoMac-Pro.local>
2017-12-01 00:31:42 +0900

    2 str

--- ryu/lib/packet/lldp.py
+++ ryu/lib/packet/lldp.py
@@ -634,7 +634,7 @@ class TimeStamp(LLDPBasicTLV):
             self.typelen = (self.tlv_type << LLDP_TLV_TYPE_SHIFT) | self.len
 
     def serialize(self):
-        return struct.pack('!H1c', self.typelen, self.timestamp)
+        return struct.pack('!H1s', self.typelen, self.timestamp)
 
 
 lldp.set_classes(lldp._tlv_parsers)
